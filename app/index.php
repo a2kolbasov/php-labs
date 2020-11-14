bec1a814ba34d9aadbbbc1b3987627bf966132fe
Aleksandr Kolbasov <kolbasov.a.a@edu.mirea.ru>
2020-11-15 01:46:09 +0300

    Список образов, test.php

---
+++ app/index.php
@@ -0,0 +1,12 @@
+<!-- Copyright © 2020 Aleksandr Kolbasov -->
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Test</title>
+</head>
+<body>
+    <?="Hi"?>
+</body>
+</html>
